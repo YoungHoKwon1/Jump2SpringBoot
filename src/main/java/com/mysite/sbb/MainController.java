5d6196969da40b979e4c90dd5b49b9979c7912ff
Young Ho Kwon <loizi@naver.com>
2024-03-23 23:07:05 +0900

    ch2.05~ch2.16
    - sort by domain
    - service
    - templates
    - url prefix
    - bootstrap
    - html layout
    - forms
    - alert modal

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-	@GetMapping("/sbb")
-	@ResponseBody
-	public String index() {
-//		System.out.println("index");
-		return "Welcome to sbb";
-	}
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
