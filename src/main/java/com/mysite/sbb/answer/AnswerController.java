372a1905f9d9088788c146f37872fee0238cdc34
Young Ho Kwon <loizi@naver.com>
2024-03-24 22:21:46 +0900

    ch3-01 ~ ch3-04
    - navbar
    - paging
    - numbering
    - showing # of answers

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 
@@ -21,7 +21,8 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id,@Valid AnswerForm answerForm, BindingResult bindingResult) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm,
+            BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -14,24 +14,33 @@ import org.springframework.transaction.annotation.Transactional;
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
-
+import com.mysite.sbb.question.QuestionService;
 
 @SpringBootTest
 class SbbApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
+    // private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
-    @Transactional
+    // @Transactional
+    // @Transactional 애너테이션을 사용하면 메서드가 종료될 때까지 DB 세션이 유지된다
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
+        // Optional<Question> oq = this.questionRepository.findById(2);
+        // assertTrue(oq.isPresent());
+        // Question q = oq.get();
 
-        List<Answer> answerList = q.getAnswerList();
+        // List<Answer> answerList = q.getAnswerList();
 
-        assertEquals(1, answerList.size());
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+        // assertEquals(1, answerList.size());
+        // assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+
+        // test data 300개 생성
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("test data:[%03d]", i);
+            String content = "내용 무";
+            this.questionService.create(subject, content);
+        }
     }
 }
