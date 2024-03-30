d21a47961b1473cd0256b36a0b812143ec9e2ed2
Young Ho Kwon <loizi@naver.com>
2024-03-30 23:42:46 +0900

    3.09~4.05

--- src/main/java/com/mysite/sbb/answer/Answer.java
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.user.SiteUser;
@@ -10,26 +11,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 @Entity
 public class Answer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(columnDefinition = "TEXT")
-    private String content;
+	@Column(columnDefinition = "TEXT")
+	private String content;
 
-    private LocalDateTime createDate; 
+	private LocalDateTime createDate;
 
-    @ManyToOne
-    private Question question; 
-    
-    @ManyToOne
-    private SiteUser author;
+	@ManyToOne
+	private Question question;
+
+	@ManyToOne
+	private SiteUser author;
+
+	private LocalDateTime modifyDate;
+	
+	@ManyToMany
+    Set<SiteUser> voter;
 }
